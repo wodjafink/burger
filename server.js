a972276442c3059a88fa1439316b3016ec955184
wodjafink <wodjafink@gmail.com>
2018-06-01 12:41:18 -0700

    Initial housekeeping for node

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+// parse application/json
+app.use(bodyParser.json());
