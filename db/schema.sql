a03e008345b61b0ec4bf7ca0a3f1de3e59fc3cbb
wodjafink <wodjafink@gmail.com>
2018-06-01 12:49:40 -0700

    Can't set this default value to a string must be 0 or 1

--- db/schema.sql
+++ db/schema.sql
@@ -6,6 +6,6 @@ USE burgers_db;
 CREATE TABLE burgers (
 	id Int (11) AUTO_INCREMENT NOT NULL,
 	burger_name VARCHAR(255) NOT NULL,
-	devoured BOOLEAN NOT NULL DEFAULT 'false',
+	devoured BOOLEAN NOT NULL DEFAULT 0,
 	PRIMARY KEY (id)
 );
