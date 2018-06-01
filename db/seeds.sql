a3d761c424ff537626608fef8123959269487027
wodjafink <wodjafink@gmail.com>
2018-06-01 12:48:28 -0700

    Have some SQL files to build our database

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheddar Burger');
+INSERT INTO burgers (burger_name) VALUES ('Jalapeno Burger');
+INSERT INTO burgers (burger_name) VALUES ('Western Bacon Burger');
