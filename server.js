9cc5fed74bb4e3991afb5cc1e2622b3ae0934f21
wodjafink <wodjafink@gmail.com>
2018-06-05 16:59:51 -0700

    Setting up the rest of the stuff, before it was a skeleton in here

--- server.js
+++ server.js
@@ -4,11 +4,18 @@ var bodyParser = require("body-parser");
 // Sets up the Express App
 // =============================================================
 var app = express();
-var PORT = process.env.PORT || 8080;
 
-// Sets up the Express app to handle data parsing
-
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
-// parse application/json
 app.use(bodyParser.json());
+app.use(express.static(process.cwd() + '/public'));
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+var router = require('./controllers/burger_controller.js');
+app.use('/', router);
+
+var port = process.env.PORT || 3000;
+app.listen(port);
